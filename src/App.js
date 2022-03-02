ae7d2c1057cf56bc7106383db4234aef4666e6e7
jiejackfan <jf6wt@virginia.edu>
2022-03-02 00:05:22 -0500

    3/1 night

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'; 
+import {Container, Grow, Grid} from '@material-ui/core'; 
 
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
-import memories from './images/memories.png';
 import useStyles from './styles';
 import {getPosts} from './actions/posts'
+import Navbar from './components/Navbar/Navbar.js';
 // hook
 import { useDispatch } from 'react-redux';
 
@@ -18,10 +18,7 @@ const App = () => {
     }, [currentId, dispatch]);
     return (
         <Container maxWidth='lg'>
-            <AppBar className={classes.appBar} position='static' color='inherit'>
-                <Typography className={classes.heading} variant="h2" align='center'>Memories</Typography>
-                <img className={classes.image} src={memories} alt='memories' height='60' />
-            </AppBar>
+            <Navbar />
             <Grow in>
                 <Container>
                     <Grid container className={classes.mainContainer} justifyContent='space-between' alignItems='stretch' spacing={3}>
